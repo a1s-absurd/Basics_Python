f2d79b410b06c8337572716ba13f45e668ead27b
student <student@wwildns.com>
2025-09-02 14:40:09 +0530

    initial commit

---
+++ 1_Basics_Class/1_Python_Files/basics.py
@@ -0,0 +1,13 @@
+#Basics python first class sem-3
+ans = 245
+athu = 399
+vfx = [23, 45, 99]
+cgi50 = 43.45
+yuvraj = ans / cgi50
+print(type(vfx))
+
+print(ans + athu)
+
+print(athu * cgi50)
+
+print(yuvraj)
